335483d3be4e661dfb036e1507b9b1dff16ee25b
Anmol_Sinha1 <Anmol_Sinha1@dell.com>
2020-01-10 15:40:46 +0530

    Converted indentation to tabs

--- Source/ILHelper/LoadArgs.cs
+++ Source/ILHelper/LoadArgs.cs
@@ -4,22 +4,22 @@ namespace Surrogate.Helpers
 {
 	internal static partial class ILHelpers
 	{
-        private static readonly OpCode[] LoadArgsOpCodes =
-        {
-            OpCodes.Ldarg_1,
-            OpCodes.Ldarg_2,
-            OpCodes.Ldarg_3
-        };
+		private static readonly OpCode[] LoadArgsOpCodes =
+		{
+			OpCodes.Ldarg_1,
+			OpCodes.Ldarg_2,
+			OpCodes.Ldarg_3
+		};
 
-        /// <summary>
-        /// <param name="Index">Base 0 Index. Indexes are automatically converted to Base 1 since they are the original index types.</para>
-        /// </summary>
+		/// <summary>
+		/// <param name="Index">Base 0 Index. Indexes are automatically converted to Base 1 since they are the original index types.</para>
+		/// </summary>
 		public static void LoadArgument(this ILGenerator IL, int Index)
-        {
-            if (Index < LoadArgsOpCodes.Length)
-                IL.Emit(LoadArgsOpCodes[Index]);
+		{
+			if (Index < LoadArgsOpCodes.Length)
+				IL.Emit(LoadArgsOpCodes[Index]);
 			else
-                IL.Emit(OpCodes.Ldarg, Index + 1);
-        }
+				IL.Emit(OpCodes.Ldarg, Index + 1);
+		}
 	}
 }
