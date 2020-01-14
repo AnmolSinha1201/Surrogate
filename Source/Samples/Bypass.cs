991733d659c6516771cb496fb6e0ee129703928e
Anmol_Sinha1 <Anmol_Sinha1@dell.com>
2020-01-14 11:53:21 +0530

    Added Bypass

---
+++ Source/Samples/Bypass.cs
@@ -0,0 +1,16 @@
+using System;
+using Surrogate.Helpers;
+using Surrogate.Interfaces;
+
+namespace Surrogate.Samples
+{
+	[AttributeUsage(AttributeTargets.Method)]
+	public class Bypass : Attribute, IMethodSurrogate
+	{
+		public void InterceptMethod(MethodSurrogateInfo Info)
+		{
+			Console.WriteLine($"Bypassing {Info.Member.Name}");
+			Info.ReturnValue = Info.Member.ReturnType.Default();
+		}
+	}
+}
