c42db570bfa472fcafc7add51c3983b3394d18a3
Anmol_Sinha1 <Anmol_Sinha1@dell.com>
2020-01-19 03:48:16 +0530

    Added tests for clamp

---
+++ Test/Samples/Clamp/Test.cs
@@ -0,0 +1,44 @@
+using System;
+using AutoFixture.Xunit2;
+using Surrogate.Samples;
+using Xunit;
+
+namespace Surrogate.Tests.ClampTest
+{
+    public class Cases
+    {
+		public Setup TestObject;
+		public Random RGen;
+		public Cases()
+		{
+			TestObject = (Setup)SurrogateBuilder.Build<Setup>();
+			RGen = new Random();
+		}
+
+		[Fact]
+        public void InputNull()
+        {
+			Assert.ThrowsAny<Exception>(() => TestObject.Method(null));
+        }
+
+		[Theory, AutoData]
+        public void InputNotNull_OutputNull(int Input)
+        {
+			Assert.ThrowsAny<Exception>(() => TestObject.Method2(Input));
+        }
+
+		[Theory, AutoData]
+        public void InputNotNull_OutputNotNull_OutputClamped(int Input)
+        {
+			var retVal = TestObject.Method3(Input);
+			Assert.Equal(retVal, Math.Clamp(Input, 5, 10));
+        }
+
+		[Theory, AutoData]
+        public void InputNotNull_OutputNotNull_InputClamped(int Input)
+        {
+			var retVal = TestObject.Method4(Input);
+			Assert.Equal(retVal, Math.Clamp(Input, 5, 10));
+        }
+    }
+}
