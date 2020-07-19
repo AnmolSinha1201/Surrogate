2731ee1a7e2390cd2b2c659f87169c06274b54bc
Anmol Sinha <anmolsinha1201@outlook.com>
2020-07-20 02:32:48 +0530

    Moved CreatePassThroughConstructors

---
+++ Source/ILAssist/Constructor.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Surrogate.ILAssist
+{
+	public class ILConstructor
+	{
+		public ILGenerator Generator;
+		public ConstructorBuilder Base;
+
+		public ILConstructor(ConstructorBuilder Builder)
+		{
+			Base = Builder;
+			Generator = Builder.GetILGenerator();
+		}
+	}
+
+	public static partial class Extensions
+	{
+		/// From : https://stackoverflow.com/questions/6879279/using-typebuilder-to-create-a-pass-through-constructor-for-the-base-class
+		/// <summary>Creates one constructor for each public constructor in the base class. Each constructor simply
+		/// forwards its arguments to the base constructor, and matches the base constructor's signature.
+		/// Supports optional values, and custom attributes on constructors and parameters.
+		/// Does not support n-ary (variadic) constructors</summary>
+		public static void CreatePassThroughConstructors(this TypeBuilder builder, Type baseType)
+		{
+			foreach (var constructor in baseType.GetConstructors()) {
+				builder.CreatePassThroughConstructor(constructor);
+			}
+		}
+
+		internal static void CreatePassThroughConstructor(this TypeBuilder Builder, ConstructorInfo Constructor)
+		{
+			var parameters = Constructor.GetParameters();
+			if (parameters.Length > 0 && parameters.Last().IsDefined(typeof(ParamArrayAttribute), false))
+				throw new InvalidOperationException("Variadic constructors are not supported");
+
+			var parameterTypes = parameters.Select(p => p.ParameterType).ToArray();
+			var requiredCustomModifiers = parameters.Select(p => p.GetRequiredCustomModifiers()).ToArray();
+			var optionalCustomModifiers = parameters.Select(p => p.GetOptionalCustomModifiers()).ToArray();
+
+			var ctor = Builder.DefineConstructor(MethodAttributes.Public, Constructor.CallingConvention, parameterTypes, requiredCustomModifiers, optionalCustomModifiers);
+			for (var i = 0; i < parameters.Length; ++i) {
+				var parameter = parameters[i];
+				var parameterBuilder = ctor.DefineParameter(i + 1, parameter.Attributes, parameter.Name);
+				if (((int)parameter.Attributes & (int)ParameterAttributes.HasDefault) != 0) {
+					parameterBuilder.SetConstant(parameter.RawDefaultValue);
+				}
+
+				foreach (var attribute in ToCustomAttributeBuilder(parameter.GetCustomAttributesData())) {
+					parameterBuilder.SetCustomAttribute(attribute);
+				}
+			}
+
+			foreach (var attribute in ToCustomAttributeBuilder(Constructor.GetCustomAttributesData())) {
+				ctor.SetCustomAttribute(attribute);
+			}
+
+			var emitter = ctor.GetILGenerator();
+			emitter.Emit(OpCodes.Nop);
+
+			// Load `this` and call base constructor with arguments
+			emitter.Emit(OpCodes.Ldarg_0);
+			for (var i = 1; i <= parameters.Length; ++i) {
+				emitter.Emit(OpCodes.Ldarg, i);
+			}
+			emitter.Emit(OpCodes.Call, Constructor);
+
+			emitter.Emit(OpCodes.Ret);
+		}
+	}
+}
