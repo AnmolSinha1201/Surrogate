b4e8b6056622aa6a06dd4ebe60e1670488484a81
Anmol Sinha <anmolsinha1201@outlook.com>
2020-07-20 22:10:01 +0530

    Added FindAttribute extensions

--- Source/ILAssist/Attributes.cs
+++ Source/ILAssist/Attributes.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
+using Surrogate.Interfaces;
 
 namespace Surrogate.ILAssist
 {
@@ -25,5 +26,23 @@ namespace Surrogate.ILAssist
 			
 			return new CustomAttributeBuilder(CustomAttribute.Constructor, attributeArgs, propertyInfos, propertyValues, namedFieldInfos, namedFieldValues);
 		}
+
+		internal static Attribute[] FindAttributes(this MethodInfo Method, Type AttributeType)
+		{
+			var retVal = new List<Attribute>();
+			var attributes = AttributeType == typeof(IReturnSurrogate) ?
+				Method.ReturnTypeCustomAttributes.GetCustomAttributes(true).Cast<Attribute>() : Method.GetCustomAttributes();
+			
+			foreach (var attribute in attributes)
+			{
+				if (AttributeType.IsAssignableFrom(attribute.GetType()))
+					retVal.Add(attribute);
+			}
+
+			return retVal.ToArray();
+		}
+
+		internal static T[] FindAttributes<T>(this MethodInfo Method)
+		=> Method.FindAttributes(typeof(T)).Cast<T>().ToArray();
 	}
 }
