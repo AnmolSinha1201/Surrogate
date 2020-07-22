4c9e90525cb54b796480e1f72eb59d8136fa6177
Anmol Sinha <anmolsinha1201@outlook.com>
2020-07-22 18:27:21 +0530

    Changed building algorithm to utilize v2 methods

--- Source/SurrogateBuilder/Builder.cs
+++ Source/SurrogateBuilder/Builder.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using Surrogate.Helpers;
+using Surrogate.ILAssist;
 using Surrogate.Interfaces;
 using Surrogate.Internal.Helpers;
 
@@ -12,72 +13,34 @@ namespace Surrogate
 	public static partial class SurrogateBuilder
 	{
 		public static Dictionary<Type, Type> Cache = new Dictionary<Type, Type>();
-		
-		public static object Build(Type ItemType, params object[] Arguments)
-		{
-			if (Cache.ContainsKey(ItemType))
-				return Activator.CreateInstance(Cache[ItemType]);
 
-			var builder = ItemType.CreateTypeBuilder();
-			// var method = ItemType.GetMethod(nameof(Foo.ActualMethod));
-			var methods = ItemType.GetMethods();
+		public static object Build(Type BaseType)
+		{
+			var builder = BaseType.ToTypeBuilder();
+			var methods = BaseType.GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly);
 			foreach (var method in methods)
+				builder.OverrideMethod(method);
+
+			return Activator.CreateInstance(builder.CreateType());
+		}
+
+		internal static void OverrideMethod(this TypeBuilder Builder, MethodInfo Method)
+		{
+			Builder.OverrideMethod(Method, il =>
 			{
-				if (!method.IsVirtual)
-					continue;
+				var backingMethod = Builder.CreateBackingMethod(Method);
 
-				MethodWorkflowDispatch(builder, method);
-			}
+				il.LoadExternalMethodInfo(backingMethod);
+				il.Emit(OpCodes.Ldarg_0);
+				il.LoadExternalMethodInfo(Method);
 
-			var type = builder.CreateType();
-			Cache.Add(ItemType, type);
-			return Activator.CreateInstance(type, Arguments);
-		}
+				var array = il.CreateArgumentsArray(Method);
+				il.Emit(OpCodes.Ldloc, array.Address);
 
-		public static object Build<T>(params object[] Arguments)
-		{
-			return Build(typeof(T), Arguments);
-		}
-
-		private static void MethodWorkflowDispatch(TypeBuilder Builder, MethodInfo Method)
-		{
-			var methodAttributes = AttributeFinder.FindAttributes(Method, typeof(IMethodSurrogate));
-			var parameterAttributes = AttributeFinder.FindAttributes(Method.GetParameters(), typeof(IParameterSurrogate));
-			var returnAttributes = AttributeFinder.FindAttributes(Method, typeof(IReturnSurrogate));
-
-			if (methodAttributes.Length == 0 && parameterAttributes.Length == 0 && returnAttributes.Length == 0)
-				return;
-			
-			Builder.OverrideMethod(Method);
-		}
-
-		private static LocalBuilder OverrideMethod(this TypeBuilder Builder, MethodInfo Method)
-		{
-			var parameterTypes = Method.GetParameters().Select(i => i.ParameterType).ToArray();
-			MethodBuilder methodBuilder = Builder.DefineMethod(
-				Method.Name,
-				Method.Attributes,
-				CallingConventions.HasThis,
-				Method.ReturnType,
-				parameterTypes
-			);
-			var backingMethod = Builder.CreateBackingMethod(Method);
-			ILGenerator il = methodBuilder.GetILGenerator();
-			var args = il.CreateParameterProxy(Method);
-			
-			var returnValue = il.CreateMethodInterceptor(Method, backingMethod, args);
-
-			if (Method.ReturnType != null && Method.ReturnType != typeof(void))
-			{
-				il.CreateReturnProxy(Method, returnValue);
-
-				il.Emit(OpCodes.Ldloc, returnValue);	
+				il.Emit(OpCodes.Call, typeof(Extensions).GetMethod(nameof(Extensions.SurrogateHook)));
 				il.Emit(OpCodes.Unbox_Any, Method.ReturnType);
-			}
-			il.Emit(OpCodes.Ret);
-
-			Builder.DefineMethodOverride(methodBuilder, Method);
-			return returnValue;
+				il.Emit(OpCodes.Ret);
+			});
 		}
 	}
 }
