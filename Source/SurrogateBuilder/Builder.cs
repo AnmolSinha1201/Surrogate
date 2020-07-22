c5ad69a9c0e7831b7f1f7344d4eaa89db4cab071
Anmol Sinha <anmolsinha1201@outlook.com>
2020-07-22 18:54:01 +0530

    Hooked up Build with Cache

--- Source/SurrogateBuilder/Builder.cs
+++ Source/SurrogateBuilder/Builder.cs
@@ -17,12 +17,24 @@ namespace Surrogate
 
 		public static object Build(Type BaseType, params object[] Params)
 		{
+			if (Cache.ContainsKey(BaseType))
+				return Activator.CreateInstance(Cache[BaseType], Params);
+
+
 			var builder = BaseType.ToTypeBuilder();
 			var methods = BaseType.GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly);
 			foreach (var method in methods)
 				builder.OverrideMethod(method);
 
-			return Activator.CreateInstance(builder.CreateType(), Params);
+			var generatedType = builder.CreateType();
+			
+			// To make it truly thread safe, we check need to check if it already exists, and if it doesn't
+			// add or update it, since multiple threads can add it at the same time, and it doesn't make sense
+			// to throw exceptions in such cases.
+			if (!Cache.ContainsKey(BaseType))
+				Cache.AddOrUpdate(BaseType, generatedType, (key, oldValue) => generatedType);
+
+			return Activator.CreateInstance(generatedType, Params);
 		}
 
 		private static void OverrideMethod(this TypeBuilder Builder, MethodInfo Method)
