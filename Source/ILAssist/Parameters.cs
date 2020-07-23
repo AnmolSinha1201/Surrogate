2893aa4803160fd7feca21d45777d797d32bca3a
Anmol Sinha <anmolsinha1201@outlook.com>
2020-07-23 16:31:41 +0530

    Moved LoadArgument

--- Source/ILAssist/Parameters.cs
+++ Source/ILAssist/Parameters.cs
@@ -9,6 +9,21 @@ namespace Surrogate.ILAssist
 {
 	public static partial class Extensions
 	{
+		private static readonly OpCode[] LoadArgsOpCodes =
+		{
+			OpCodes.Ldarg_0,
+			OpCodes.Ldarg_1,
+			OpCodes.Ldarg_2,
+			OpCodes.Ldarg_3
+		};
+		public static void LoadArgument(this ILGenerator Generator, int Index)
+		{
+			if (Index <= LoadArgsOpCodes.Length)
+				Generator.Emit(LoadArgsOpCodes[Index]);
+			else
+				Generator.Emit(OpCodes.Ldarg, Index);
+		}
+		
 		internal static void ApplyParameters(this ConstructorBuilder Constructor, ParameterInfo[] OriginalParameters)
 		{
 			for (var i = 0; i < OriginalParameters.Length; i++) 
